7c7cd328281ce106b38d8fb3cb737dd6cb492808
Simo Angelov <simeon_g_angelov@abv.bg>
2017-07-07 17:43:55 +0300

    Adding win calculations and getters for the model

--- Game/GameModel.h
+++ Game/GameModel.h
@@ -11,23 +11,33 @@
 #include "Globals.h"
 class GameModel
 {
-friend class GameController;
+	friend class GameController;
 private:
-	int m_iBet;
-	int m_iMaxBet;
+	int m_iNumberOfLines;
+	int m_iBetPerLine;
 	int m_iWin;
 	int m_iCredits;
-
+	int m_iTotalBet;
 protected:
-	static int m_iGameReels[GAME_ROWS][GAME_REELS];
+	static Figures m_iGameReels[GAME_ROWS][GAME_REELS];
 	static vector<Payline> m_vecPaylines;
 
 public:
-	GameModel(int iBet = 0, int iMax = 0,
-			int iWin = 0, int iCredits = 0);
+	GameModel(int iNumberOfLines = 1, int iBetPerLine = 1,
+			int iWin = 0, int iCredits = 0, int iTotalBet = 1);
 	static void InitReels();
 	static void InitVecPaylines();
 	virtual ~GameModel();
+	int GetIBetPerLine() const;
+	void SetIBetPerLine(int iBetPerLine);
+	int GetICredits() const;
+	void SetICredits(int iCredits);
+	int GetINumberOfLines() const;
+	void SetINumberOfLines(int iNumberOfLines);
+	int GetITotalBet() const;
+	void SetITotalBet();
+	int GetIWin() const;
+	void SetIWin(int iWin);
 };
 
 #endif /* GAMEMODEL_H_ */
--- Game/mainTest.cpp
+++ Game/mainTest.cpp
@@ -13,10 +13,27 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	GameController game;
-	game.InitCurrentReels();
-	game.InitCurrentPayLines();
+	cout << "EnterCredits" << endl;
+	int credits;
+	cin >> credits;
+	game.InsertCredits(credits);
+	cout << "Enter Number Of Lines: " << endl;
+	int numberOfLines;
+	cin >> numberOfLines;
+	game.SetNumberOfPaylines(numberOfLines);
+	cout << "set steps for bet" << endl;
+	int step;
+	cin >> step;
+	game.SetBetPerPayline(step);
+
+	game.Spin();
+//	game.InitCurrentReels();
+//	//game.InitCurrentPayLines();
+//	game.InitCurrentPaylines();
 	game.PrintReels();
 	cout << endl;
 	game.PrintPaylines();
+	cout << endl;
+	game.WinFromPaylines();
 	return 0;
 }
